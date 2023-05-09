9ab80a1efa15298fd449b6a34e1b31cda370fe6c
Igor Karpov <70741515+karpovigorev@users.noreply.github.com>
2023-05-09 15:48:43 +0300

    1

--- VK/friends_project/friends_project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('friends_app.urls')),
-    path('', TemplateView.as_view(template_name='home.html')),
-]
